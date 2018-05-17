525424606869d34f140a1f33f9a5dbfc55736b59
黃仕杰 <huangshijie@huangshijiede-macbook-pro.local>
2018-05-17 22:50:32 +0800

    Initial Commit

---
+++ SwiftBT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBT
+//
+//  Created by 黃仕杰 on 2018/5/17.
+//  Copyright © 2018年 BT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
